f74d3fc99c17245337276c9b9b125458a4ecb911
josimar lourenço dev <josimarlourencodev@gmail.com>
2026-03-19 15:42:19 -0300

    Adicionada a rota de listagem de produtos incompleta.

--- pharmacy_api/app/routes/produto_routes.py
+++ pharmacy_api/app/routes/produto_routes.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from sqlalchemy import text 
 
 produto_router = APIRouter(prefix="/produto", tags=["produto"])
 
@@ -6,6 +7,11 @@ produto_router = APIRouter(prefix="/produto", tags=["produto"])
 async def produto():
     return {"mensagem": "Você acessou a rota de produtos"}
 
-@produto_router("/listar")
-async def listar():
-    text = ()
+
+
+# A ideia é que ele liste o produto com id de parâmetro da função
+@produto_router.get("/listar") # Eu não sei como fazer isso ainda da forma que estou pensando, parece que vou ter que fazer um schema com Pydantic.  
+def listar():
+   """ listar_produtos = text('select id_produto, medicamento, preco from produtos') 
+    con.execute(listar_produtos)"""
+pass 
